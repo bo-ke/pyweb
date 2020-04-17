75adcb0a6bc376799891e26fdfb3263f48e193d0
kebo <itachi971009@gmail.com>
2020-04-17 23:57:36 +0800

    [fea] init commit

---
+++ project_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: SpongeBob
+@contact: itachi971009@gmail.com
+@version: 1.0
+@file: project_config.py
+@time: 2020/04/17 23:57
+这一行开始写关于本文件的说明与解释
+"""
+import os
+
+PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def func():
+    pass
+
+
+if __name__ == '__main__':
+    func()
